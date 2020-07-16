6bd177d55b0737d5ac934ff06e4d1dba3251594a
sam <sam.purkis@hotmail.co.uk>
2020-07-17 00:29:38 +0100

    [doorbell project] adding text to speech system

---
+++ text_to_speech/text_to_speech.py
@@ -0,0 +1,34 @@
+import gtts
+from pathlib import Path
+import playsound
+import pyttsx3
+import vlc
+
+
+def tts(text, offline=False, **kwargs):
+    if offline:
+        tts_offline(text)
+    else:
+        try:
+            tts_online(text, **kwargs)
+        except gtts.gTTSError:
+            tts_offline(text)
+
+
+def tts_online(text, **kwargs):
+    if kwargs.get("names"):
+        file_path = Path("audio_files", f"""{"-".join(kwargs.get('names'))}.mp3""")
+        kwargs.pop("names")
+    _tts = gtts.gTTS(text, **kwargs)
+    if not file_path.exists():
+        _tts.save(str(file_path))
+        print("saving file", str(file_path))
+    p = vlc.MediaPlayer(str(file_path))
+    p.play()
+    # playsound.playsound(str(file_path))
+
+
+def tts_offline(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
